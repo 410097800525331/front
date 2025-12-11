68a3a56b3725f3c419c5a0c7241b7e36422b0d61
김슬기 <xxx696193@gmail.com>
2025-12-11 09:37:12 +0900

    1211

---
+++ megabox/js/common.js
@@ -0,0 +1,16 @@
+// header 2depth background
+// 스토어 메뉴는 하위 메뉴가 없으므로 제외
+$('.main li:not(:nth-of-type(5)').mouseenter((e) => {
+  $('#header').addClass('active');
+});
+$('.main li').mouseleave(() => {
+  $('#header').removeClass('active');
+});
+
+// footer - looking for theater
+$(".btn_looking_theater").on("click", () => {
+  $(".theater").addClass("active");
+});
+$(".closed").on("click", () => {
+  $(".theater").removeClass("active");
+});
--- megabox/js/main.js
+++ megabox/js/main.js
@@ -1,45 +1,35 @@
-// Movie Card Ui
-datas.forEach(data => {
-  const screenImgs = data.screenTypes.map(ele => `<img src="${ele}" alt="Screen Types">`).join('');
+// main.js
+const formatCount = (num) => num >= 1000 ? (num / 1000).toFixed(1) + "k" : num;
+
+// Movie Card UI
+$(datas).each((idx, data) => {
+  const screenImgs = data.screenTypes
+    .map((ele) => `<img src="${ele}" alt="Screen Types">`)
+    .join("");
 
   const card = `<div class="box_office_card">
-      <div class="inner">
-        <!-- front -->
-        <div class="front">
-          <span class="rank">${data.rank}</span>
-          <div class="poster">
-            <img src="${data.poster}" alt="${data.title}">
-          </div>
-          <div class="screen_type">
-            ${screenImgs}
-          </div>
-          <div class="grade">
-            <img src="${data.grade}" alt="${data.age}">
-          </div>
-        </div>
-        <!-- back -->
-        <div class="back">
-          <p>
-            ${data.description}
-          </p>
-          <div class="score">관람평<span>${data.score}</span></div>
-        </div>
-      </div>
-      <!-- btn -->
-      <div class="btn">
-        <button type="button" class="like"><i class="fa-regular fa-heart"></i>${data.like}</button>
-        <a href="#" class="reservation_link">예매</a>
-      </div>
-    </div>`
+            <div class="inner">
+                <div class="front">
+                    <span class="rank">${data.rank}</span>
+                    <div class="poster">
+                        <img src="${data.poster}" alt="${data.title}">
+                    </div>
+                    <div class="screen_type">${screenImgs}</div>
+                    <div class="grade">
+                        <img src="${data.grade}" alt="Grade ${data.age}">
+                    </div>
+                </div><!-- //.front -->
+                <div class="back">
+                    <p class="description">${data.description}</p>
+                    <div class="score">관람평<span>${data.score}</span></div>
+                </div><!-- //.back -->
+            </div><!-- //.inner -->
+            <!-- btn -->
+            <div class="btn">
+                <button type="button" class="like"><i class="fa-regular fa-heart"></i>${formatCount(data.like)}</button>
+                <a href="" class="reservation_link">예매</a>
+            </div>
+        </div>`;
 
-  $('.box_office').append(card);
-
-});
-
-// footer btn
-$('.btn_looking_theater').on('click', () => {
-  $('.theater, .btn_looking_theater').addClass('active');
-});
-$('.closed').on('click', () => {
-  $('.theater, .btn_looking_theater').removeClass('active');
-});
+  $(".box_office").append(card);
+}); // each()
